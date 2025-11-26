dbd1697b3cc1cf33987e14dd752fa8a6e48cebaf
Wilson Porteus <103076735+pineapplefart@users.noreply.github.com>
2025-11-26 09:11:09 +0000

    added simulation files, runlog, and block file that has the run function

---
+++ simulate_protocol_runlog.py
@@ -0,0 +1,16 @@
+import opentrons.simulate
+from opentrons.simulate import format_runlog 
+
+
+# read the file 
+
+protocol_file = open('/Users/wilsonporteus/Documents/GitHub/ssb_s5/main.py') 
+
+# simulate() the protocol, keeping the runlog 
+#opentrons.simulate.simulate(protocol_file) 
+
+runlog, _bundle = opentrons.simulate.simulate(protocol_file) 
+
+# print the runlog 
+
+print(format_runlog(runlog)) 
