d3f592e587e33cd823805b00c7d41cf375a0b451
Wilson Porteus <103076735+pineapplefart@users.noreply.github.com>
2025-12-01 13:59:14 +0000

    Update simulate_protocol_runlog.py

--- Serial_Dilution/simulate_protocol_runlog.py
+++ Serial_Dilution/simulate_protocol_runlog.py
@@ -4,7 +4,7 @@ from opentrons.simulate import format_runlog
 
 # read the file 
 
-protocol_file = open('/Users/wilsonporteus/Documents/GitHub/ssb_s5/calibration.py') 
+protocol_file = open('/Users/wilsonporteus/Documents/GitHub/ssb_s5/Serial_Dilution/main.py') 
 
 # simulate() the protocol, keeping the runlog 
 #opentrons.simulate.simulate(protocol_file) 
